3a8a02426d27d07f24b182a2bff339f83d5b809e
Alex Gaynor <alex.gaynor@gmail.com>
2021-08-14 09:04:38 -0400

    Revert "Revert "Make OID storage fixed length - this will make it possible for them to be structurally equal (#180)" (#182)"
    This reverts commit f613e5c8dd3767d8543444a97857986843e7de4a.

--- src/parser.rs
+++ src/parser.rs
@@ -21,6 +21,9 @@ pub enum ParseErrorKind {
     InvalidSetOrdering,
     /// An OPTIONAL DEFAULT was written with a default value.
     EncodedDefault,
+    /// OID value is longer than the maximum size rust-asn1 can store. This is
+    /// a limitation of rust-asn1.
+    OidTooLong,
 }
 
 #[derive(Debug, PartialEq)]
@@ -566,7 +569,7 @@ mod tests {
 
     #[test]
     fn test_parse_object_identifier() {
-        assert_parses::<ObjectIdentifier<'_>>(&[
+        assert_parses::<ObjectIdentifier>(&[
             (
                 Ok(ObjectIdentifier::from_string("2.5").unwrap()),
                 b"\x06\x01\x55",
